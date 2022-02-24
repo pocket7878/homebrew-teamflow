ed6fcd1bbfab3665ae982e36762d48932145e605
Pocket7878 <poketo7878@gmail.com>
2022-02-24 11:58:50 +0900

    feature: Support arm64 mac

--- Casks/teamflow.rb
+++ Casks/teamflow.rb
@@ -1,9 +1,17 @@
 cask "teamflow" do
-  version "32.0.1"
+  version "HEAD"
   sha256 :no_check
 
-  url "https://huddle-production.sfo2.digitaloceanspaces.com/desktop-apps/Teamflow-latest.dmg",
-      verified: "huddle-production.sfo2.digitaloceanspaces.com/desktop-apps/Teamflow-latest.dmg"
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://huddle-production.sfo2.digitaloceanspaces.com/desktop-apps/Teamflow-latest-x64.dmg",
+          verified: "huddle-production.sfo2.digitaloceanspaces.com/desktop-apps/Teamflow-latest-x64.dmg"
+    else
+      url "https://huddle-production.sfo2.digitaloceanspaces.com/desktop-apps/Teamflow-latest-arm64.dmg",
+          verified: "huddle-production.sfo2.digitaloceanspaces.com/desktop-apps/Teamflow-latest-arm64.dmg"
+    end
+  end
+
   name "teamflow"
   desc "Single place for virtual meetings, brainstorming and collaboration"
   homepage "https://www.teamflowhq.com/"
